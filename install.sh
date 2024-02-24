21204b38d40156d9ee262644c1a3a0fde0c3a0f4
nkundinezayv <115110072+nkundinezayv@users.noreply.github.com>
2024-02-24 05:18:41 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '###---------- free-spotify ----------###' >> /etc/hosts
-curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/hosts >> /etc/hosts
+curl https://raw.githubusercontent.com/nkundinezayv/ADhosts/main/hosts >> /etc/hosts
 echo '###---------- made by @harrytran103 ----------###' >> /etc/hosts
